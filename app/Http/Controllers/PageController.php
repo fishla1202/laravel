618822a514ec1700068b2b8524f9b1f46cc25972
Chen Yu,Xiang <peterleo1202@gmail.com>
2018-08-10 17:27:14 +0800

    route knowhow

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PageController extends Controller 
+{
+    public function about() 
+    {
+        return "About Us Page";
+    }
+
+    public function contact() {
+        return "Contact Page";
+    }
+}
