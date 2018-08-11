e32454534c6ef1b1c8c76526eb72b7c9b5ca5d52
Chen Yu,Xiang <peterleo1202@gmail.com>
2018-08-12 03:38:05 +0800

    laravel 驗證方法 寫入資料庫 寫入成功後導回頁面

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Question;
 
 class QuestionController extends Controller
 {
@@ -23,7 +24,7 @@ class QuestionController extends Controller
      */
     public function create()
     {
-        //
+        return view("questions.create");
     }
 
     /**
@@ -34,7 +35,24 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // 驗證 輸入格式有沒有符合 沒有符合會自動導回表單頁面
+        $this->validate($request, [
+            'title' => 'required|max:255'
+        ]);
+
+        //寫入資料庫 先創建一個 question的模型 再將驗證過的用戶資料寫入
+        $question = new Question();
+        $question->title = $request->title;
+        $question->description = $request->description;
+
+        // 如果成功寫入 就轉去show得頁面 php artisan route:list
+        if ($question->save()) {
+            return redirect()->route('questions.show', $question->id);
+        }else {
+            return redirect()->route('questions.create');
+        }
+        
+
     }
 
     /**
